58edfbf58b63f0e7d200d04d1101eb31b64e3099
hooneats <hooneats@naver.com>
2022-10-22 21:29:40 +0900

    spring-transaction propagation

--- spring-transaction/src/test/java/org/example/spring/SpringApplicationTest.java
+++ spring-transaction/src/test/java/org/example/spring/SpringApplicationTest.java
@@ -9,6 +9,13 @@ class SpringApplicationTest {
 
     /**
      * TODO :
+     *  트랜젝션 전파와 옵션
+     *   'isolation' , 'timeout' , 'readOnly' 는 트랜젝션이 처음 시작될때만 적용되기에, 트랜젝션에 참여하는 경우에는 적용되지 않는다.
+     *      ㄴ 예를 들어 propagation 이 'REQUIRED' 를 통한 생성 시점, 또는 'REQUIRES_NEW' 를 통해 새로 생성한 시점에만 적용된다.
+     */
+
+    /**
+     * TODO :
      *  @Transactional 의 옵션
      *      - value , transactionManager
      *      - rollbackFor : 기본은 RuntimeException 과 Error  그 하위 를 롤백 (Exception 과 그 하위는 커밋한다.)
@@ -28,6 +35,22 @@ class SpringApplicationTest {
      *              ㄴ 프레임워크 - readOnly = true 인 경우, JPA 는 플러시를 하지 않는다.
      *              ㄴ JDBC 드라이버 - readOnly = true 인 경우, 변경 쿼리가 발생하면 예외를 던지거나 또는 읽기 커넥션을 맺는다.(이는 DB와 드라이버에 따라 다르다.)
      *              ㄴ 데이터베이스 - readOnly = true 인 경우, 내부에서 성능 최적화가 발생한다.(이는 DB와 드라이버에 따라 다르다.)
+     */
+
+    /**
+     * TODO :
+     *  propagation 옵션
+     *      - REQUIRED : 기존 트랜젝션이 있으면 참여 없으면 생성
+     *      - REQUIRES_NEW : 항상 새로운 트랜젝션 생성
+     *      - SUPPORT : 기존 트랜젝션이 없으면 없는대로 진행, 있으면 참여
+     *      - NOT_SUPPORT : 항상 트랜젝션 없이 진행. 기존 트랜젝션이 있을경우 이를 보류
+     *      - MANDATORY : 의무사항, 트랜젝션이 반드시 있어야한다. 기존 트랜젝션이 없으면 에외가 발생
+     *      - NEVER : 트랜젝션을 사용하지 않는다. 이는 기존 트랜젝션이 있으면 예외 발생
+     *      - NESTED : 기존 트랜젝션이 없으면 생성, 있으면 중첩 트랜젝션을 만든다.
+     *             ㄴ 중첩 트랜젝션은 외부 트랜젝션의 영향을 받지만, 외부에 영향을 주지 않는다.
+     *              ㄴ 중첩 트랜젝션이 롤백 되어도 외부 트랜젝션은 커밋 가능
+     *               ㄴ 외부 트랜젝션이 롤백 되면 중첩 트랜젝션도 함께 롤백
+     *               ㄴ 이는 JPA 사용할 떄 사용불가, JDBC savepoint 기능을 사용한다. 필요시 DB 드라이버가 해당 기능 지원하는지 확인필요
      */
 
 }
---
+++ spring-transaction/src/test/java/org/example/spring/propagation/BasicTxTest.java
@@ -0,0 +1,180 @@
+package org.example.spring.propagation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.UnexpectedRollbackException;
+import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
+
+import javax.sql.DataSource;
+
+@Slf4j
+@SpringBootTest
+public class BasicTxTest {
+
+    @Autowired
+    PlatformTransactionManager txManager;
+
+    @TestConfiguration
+    static class Config {
+        @Bean
+        public PlatformTransactionManager transactionManager(DataSource dataSource) {
+            return new DataSourceTransactionManager(dataSource);
+        }
+    }
+
+    @DisplayName("트랜젝션 커밋")
+    @Test
+    void commit() {
+        log.info("트랜젝션 시작");
+        TransactionStatus status = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("트랜젝션 커밋 시작");
+        txManager.commit(status);
+        log.info("트랜젝션 커밋 완료");
+    }
+
+    @DisplayName("트랜젝션 롤백")
+    @Test
+    void rollback() {
+        log.info("트랜젝션 시작");
+        TransactionStatus status = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("트랜젝션 롤백 시작");
+        txManager.rollback(status);
+        log.info("트랜젝션 롤백 완료");
+    }
+
+    @DisplayName("트랜젝션 두번 사용")
+    @Test
+    void double_commit() {
+        log.info("트랜젝션 시작");
+        TransactionStatus tx1 = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("트랜젝션 커밋 시작");
+        txManager.commit(tx1);
+        log.info("트랜젝션 커밋 완료");
+
+        log.info("트랜젝션 시작");
+        TransactionStatus tx2 = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("트랜젝션 커밋 시작");
+        txManager.commit(tx2);
+        log.info("트랜젝션 커밋 완료");
+    }
+
+    @DisplayName("트랜젝션 커밋 and 롤백")
+    @Test
+    void double_commit_rollback() {
+        log.info("트랜젝션 시작");
+        TransactionStatus tx1 = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("트랜젝션 커밋 시작");
+        txManager.commit(tx1);
+        log.info("트랜젝션 커밋 완료");
+
+        log.info("트랜젝션 시작");
+        TransactionStatus tx2 = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("트랜젝션 롤백 시작");
+        txManager.rollback(tx2);
+        log.info("트랜젝션 롤백 완료");
+    }
+
+    @DisplayName("트랜젝션 내부커밋 in 외부커밋")
+    @Test
+    void inner_commit() {
+        log.info("외부 트랜젝션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction() = {}", outer.isNewTransaction()); // true
+
+        log.info("내부 트랜젝션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        // ***** 외부에 Tx 있기에 false 가 된다. 즉 외부와 내부가 하나로 묶인다는(전파되었다는) 뜻.
+        // 트랜젝션 동기화 매니저에 외부 트랜젝션이 생성한 커넥션이 이미 있기에 이를 사용한다.
+        log.info("inner.isNewTransaction() = {}", inner.isNewTransaction()); // false
+        log.info("내부 트랜젝션 커밋 시작");
+        // 내부 커밋에 대한 아무런 로그가 찍히지 않는다.
+        // 이는 외부 트랜젝션에 참여한 형태(inner.isNewTransaction()=false)이기 때문에 외부 트랜젝션이 커밋할때 같이 일어난다.
+        txManager.commit(inner);
+        log.info("내부 트랜젝션 커밋 완료");
+
+        log.info("외부 트랜젝션 커밋 시작");
+        txManager.commit(outer);
+        log.info("외부 트랜젝션 커밋 완료");
+    }
+
+    @DisplayName("트랜젝션 내부커밋 in 외부롤백")
+    @Test
+    void outer_rollback() {
+        log.info("외부 트랜젝션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction() = {}", outer.isNewTransaction()); // true
+
+        log.info("내부 트랜젝션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("inner.isNewTransaction() = {}", inner.isNewTransaction()); // false
+        log.info("내부 트랜젝션 커밋 시작");
+        txManager.commit(inner);
+        log.info("내부 트랜젝션 커밋 완료");
+
+        log.info("외부 트랜젝션 롤백 시작");
+        txManager.rollback(outer);
+        log.info("외부 트랜젝션 롤백 완료");
+    }
+
+    // TODO 예외발생
+    //  UnexpectedRollbackException: Transaction rolled back because it has been marked as rollback-only
+    //  즉 기존 트랜젝션에 내부 트랜젝션이 롤백을 마킹해 놓는다.
+    @DisplayName("트랜젝션 내부롤백 in 외부커밋")
+    @Test
+    void inner_rollback() {
+        log.info("외부 트랜젝션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction() = {}", outer.isNewTransaction()); // true
+
+        log.info("내부 트랜젝션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("inner.isNewTransaction() = {}", inner.isNewTransaction()); // false
+        log.info("내부 트랜젝션 롤백 시작");
+        txManager.rollback(inner);
+        log.info("내부 트랜젝션 롤백 완료");
+
+        log.info("외부 트랜젝션 커밋 시작");
+        Assertions.assertThatThrownBy(() -> txManager.commit(outer))
+                .isInstanceOf(UnexpectedRollbackException.class);
+//        txManager.commit(outer);
+        log.info("외부 트랜젝션 커밋 완료");
+    }
+
+    // TODO
+    //  DEBUG 12900 --- [main] o.s.j.d.DataSourceTransactionManager : Suspending current transaction, creating new transaction with name [null]
+    //   ㄴ-> 즉 외부에서 만든 커넥션을 잠시 미뤄두고 새로 생성해서 사용한다. (Suspending current transaction)
+    //   ㄴ 그러나 이렇게 내부 커넥션을 새로 쓰는것은 프로그램의 커넥션 부족하게 만들 수 있기에 실시간 이용자가 많은경우 조심해야한다.
+    @DisplayName("트랜젝션 내부롤백 in 외부커밋, REQUIRES_NEW")
+    @Test
+    void inner_rollback_requires_new() {
+        log.info("외부 트랜젝션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction() = {}", outer.isNewTransaction()); // true
+
+        log.info("내부 트랜젝션 시작");
+        DefaultTransactionAttribute definition = new DefaultTransactionAttribute();
+        definition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+        TransactionStatus inner = txManager.getTransaction(definition);
+        log.info("inner.isNewTransaction() = {}", inner.isNewTransaction()); // true
+
+        log.info("내부 트랜젝션 롤백 시작");
+        txManager.rollback(inner);
+        log.info("내부 트랜젝션 롤백 완료");
+
+        log.info("외부 트랜젝션 커밋 시작");
+        txManager.commit(outer);
+        log.info("외부 트랜젝션 커밋 완료");
+    }
+}

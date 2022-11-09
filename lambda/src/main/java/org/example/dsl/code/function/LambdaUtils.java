4dc1ef37728a86a0a91bda36fc8a4a224a361248
hooneats <mz_hooneats@woowafriends.com>
2022-11-09 13:48:34 +0900

    LambdaUtils

--- lambda/src/main/java/org/example/dsl/code/function/LambdaUtils.java
+++ lambda/src/main/java/org/example/dsl/code/function/LambdaUtils.java
@@ -6,30 +6,11 @@ import java.util.stream.Stream;
 
 public final class LambdaUtils<T> {
 
-    public static <V> Lambda<V> object(V target) {
-        return new Lambda<>(Optional.ofNullable(target), Stream.empty());
+    public static <V> Optional<V> toOptional(V target) {
+        return Optional.ofNullable(target);
     }
 
-    public static <V> Lambda<V> collection(Collection<V> targets) {
-        return new Lambda<>(Optional.empty(), targets.stream());
-    }
-
-    public static class Lambda<V> {
-
-        private final Optional<V> optionalV;
-        private final Stream<V> vStream;
-
-        public Lambda(Optional<V> optionalV, Stream<V> vStream) {
-            this.optionalV = optionalV;
-            this.vStream = vStream;
-        }
-
-        public Optional<V> optional() {
-            return optionalV;
-        }
-
-        public Stream<V> stream() {
-            return vStream;
-        }
+    public static <V> Stream<V> toStream(Collection<V> targets) {
+        return targets.stream();
     }
 }

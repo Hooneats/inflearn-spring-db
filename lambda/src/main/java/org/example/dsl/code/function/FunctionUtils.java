c5a6929aa21a94a45f3b44ebb95c707b1db9c326
hooneats <mz_hooneats@woowafriends.com>
2022-11-09 14:58:58 +0900

    LambdaUtils

--- lambda/src/main/java/org/example/dsl/code/function/FunctionUtils.java
+++ lambda/src/main/java/org/example/dsl/code/function/FunctionUtils.java
@@ -8,7 +8,7 @@ import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
-public final class LambdaUtils {
+public final class FunctionUtils {
 
     public static <T> Optional<T> optional(T target) {
         return Optional.ofNullable(target);
@@ -18,19 +18,19 @@ public final class LambdaUtils {
         return targets.stream();
     }
 
-    public static <T, R> R function(T resource , Function<T, R> function) {
+    public static <T, R> R of(T resource , Function<T, R> function) {
         return function.apply(resource);
     }
 
-    public static <T> Boolean predicate(T value, Predicate<T> predicate) {
+    public static <R> R of(Supplier<R> supplier) {
+        return supplier.get();
+    }
+
+    public static <T> Boolean check(T value, Predicate<T> predicate) {
         return predicate.test(value);
     }
 
-    public static <T> void consumer(T resource, Consumer<T> consumer) {
+    public static <T> void consume(T resource, Consumer<T> consumer) {
         consumer.accept(resource);
-    }
-
-    public static <R> R supplier(Supplier<R> supplier) {
-        return supplier.get();
     }
 }
